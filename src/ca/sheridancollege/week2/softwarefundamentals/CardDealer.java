feaa793fa5e9af6ff1e25276c8bdd5e42f5cc090
eric mogielnicki <mogielne@sheridancollege.ca>
2022-05-31 12:34:00 -0400

    creating object

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -7,13 +7,13 @@ package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
  * The class where we create Cards
- * @author dancye, 2019
+ * @author mogielne 991672549, 2022
  */
 public class CardDealer 
 {
     public static void main(String[] args)
     {
-        Card nineClubs = new Card("Clbs", 9);
-        nineClubs.setSuit("diamonds");
+        Card tenClubs = new Card("Clbs", 10);
+        tenClubs.setSuit("diamonds");
     }
 }
